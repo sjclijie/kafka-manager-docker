84041186accbf3493d3c431b43331243948d51bc
zhouyq <zyq916@gmail.com>
2016-05-13 16:44:49 +0800

    add app env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 [ $DEBUG ] && set -x
 
+ZK_HOST=${ZK_HOST:-127.0.0.1}
+ZK_PORT=${ZK_PORT:-2182}
+ZK_HOSTS="$ZK_HOST:$ZK_PORT"
+APPLICATION_SECRET="$KM_PASS"
+
 for i in {30..0}; do
   if nc -w 1  -v $ZK_HOST $ZK_PORT; then
   break
